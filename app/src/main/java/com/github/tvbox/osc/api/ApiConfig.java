92fa9c0a5f41f31fec55b539575ab9608bd37913
diabloli <108588064+diabloli@users.noreply.github.com>
2022-08-12 14:53:26 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -80,7 +80,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://gitea.com/qiaoji/jar/raw/branch/main/qiaoji.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
